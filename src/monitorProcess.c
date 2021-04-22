a02cee014b7dfd22e68932da1a92bcc310a385fc
GEWRGIA KOUTIVA <gina101@windowslive.com>
2021-04-22 21:23:36 +0300

    Possibly creation and open sequence of pipes settled.
    A lot of code was written, most for the send of country names over the pipes, but it is not tested yet...
    Select for the child processes until they receive something from the parent sounds like a viable option.

--- src/monitorProcess.c
+++ src/monitorProcess.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 int main(int argc, char *argv[]){
 	// Arguments passed through exec:
@@ -16,25 +17,75 @@ int main(int argc, char *argv[]){
 	// Open the appropriate pipe for current process to read and parent process to write.
 	char *writePipeName = malloc(11*sizeof(char));
 	char *readPipeName = malloc(11*sizeof(char));
-	sprintf(readPipeName, "pipes/%dw", atoi(argv[1]));
-	if(mkfifo(readPipeName, 0666) < 0){
-        	fprintf(stderr, "errno: %d\n", errno);
-        	perror("mkfifo");
-      	}
+	strcpy(readPipeName, argv[4]);
+	strcpy(writePipeName, argv[3]);
 	int readfd = open(readPipeName, O_RDONLY | O_NONBLOCK);
         // Open the appropriate pipe created by parent for them to read and current process to write.
-	sprintf(writePipeName, "pipes/%dr", atoi(argv[1]));
 	int writefd = open(writePipeName, O_WRONLY);
+	int bufferSize = atoi(argv[2]);
+	// char *writePipeBuffer = malloc(bufferSize*sizeof(char));
+	sleep(5);
+	// Notifying parent that pipe where country names will be written is ready
 	// Create log file of current process and wait to receive country names from parent.
-	pid_t mypid = getpid();
+	/* pid_t mypid = getpid();
 	char *logFileName = malloc(14*sizeof(char));
 	sprintf(logFileName, "log_file.%d", mypid);
-//	FILE *logfile = fopen(logFileName, "w");
-
-//	fclose(logfile);
+	// About to read country names that this monitor will process
+	int countriesLength = 10;
+	char **countries = malloc(countriesLength*sizeof(char *)); // we assume that each monitor will read 10 directories.
+	int i;
+	for(i = 0; i < countriesLength; i++){
+		countries[i] = calloc(255, sizeof(char));
+	}
+	// If there are more, we will resize the array.
+	int charactersRead, charactersParsed, countryIndex = 0, done = 0;
+	while(!done){
+		// printf("One reading operation about to be done.\n");
+		if((charactersRead = read(readfd, readPipeBuffer, bufferSize)) < 0){
+                    	perror("read");
+                }
+		for(i = 1; i < charactersRead; i++){
+			strncat(countries[countryIndex], readPipeBuffer + i, 1);
+			printf("Character parsed: %c\n", readPipeBuffer[i]);
+			// when we reach a terminal character, we have parsed one country name
+			// and we must move to the next
+			if(readPipeBuffer[i] == '\0'){
+				if(strcmp(countries[countryIndex], "END") == 0){
+					memset(countries[countryIndex], 0, 3);
+					countryIndex--;
+					done = 1;
+					break;
+				}
+				memmove(readPipeBuffer, readPipeBuffer + i, bufferSize - i);
+				countryIndex++;
+				if(countryIndex == countriesLength){
+					char **temp = realloc(countries, 2*countriesLength);
+					for(i = countriesLength; i < 2*countriesLength; i++){
+						countries[i] = calloc(255, sizeof(char));
+					}
+					assert(temp != NULL);
+					countries = temp;
+					countriesLength *= 2;
+				}
+			}
+		}
+	} 
+	FILE *logfile = fopen(logFileName, "w");
+	for(i = 0; i < countryIndex; i++){
+		fprintf(logfile, "%s", countries[i]);
+	}
+	assert(fclose(logfile) == 0);
+	for(i = 0; i < countriesLength; i++){
+		free(countries[i]);
+		countries[i] = NULL;
+	}
+	free(countries);*/
 	close(writefd);
 	close(readfd);
-	free(logFileName);
-	free(writePipeName);
-	free(readPipeName);
+	//free(logFileName);
+//	free(writePipeName);
+//	free(readPipeName);
+	// free(readPipeBuffer);free(writePipeBuffer);
+	// printf("Exiting...\n");
+	return 1;
 }
--- src/travelMonitor.c
+++ src/travelMonitor.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,6 +20,7 @@ int main(int argc, char *argv[]){
   int i;
   int numMonitors, bufferSize, sizeOfBloom;
   char *input_dir = malloc(255*sizeof(char));
+  char *bufferSizeArg;
   for(i = 1; i < 9; i += 2){
     if(strlen(argv[i]) != 2){
       printf("Invalid flag %s\n", argv[i]);
@@ -28,6 +30,8 @@ int main(int argc, char *argv[]){
       case 'm': numMonitors = atoi(argv[i+1]);
             break;
       case 'b': bufferSize = atoi(argv[i+1]);
+		bufferSizeArg = malloc((strlen(argv[i+1])+1)*sizeof(char));
+		strcpy(bufferSizeArg, argv[i+1]);
             break;
       case 's': sizeOfBloom = atoi(argv[i+1]);
             break;
@@ -49,13 +53,27 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "errno: %d\n", errno);
         perror("mkfifo");
       }
+      printf("About to make pipe for reading\n");
       read_file_descs[i-1] = open(pipe_name, O_RDONLY | O_NONBLOCK);
       if(read_file_descs[i-1] < 0){
-        perror("open");
+        perror("open read pipe");
+      }
+      sprintf(pipe_name, "pipes/%dw", i);
+      if(mkfifo(pipe_name, 0666) < 0){
+        fprintf(stderr, "errno: %d\n", errno);
+        perror("mkfifo");
       }
   }
   pid_t pid;
-  char *path = "monitorProcess";
+  // Creating monitor processes and passing them their arguments
+  // Their id number, that will be used for quick reference to one after
+  // we have queried its bloom filter and we wish to query its skiplist,
+  // the bufferSizeArgument, and the pipes for communication.
+  // readPipe is the pipe from which the parent reads and the monitor writes to.
+  // writePipe is the pipe for the opposite direction of communication.
+  char *path = "monitorProcess"; 
+  char *readPipe = malloc(11*sizeof(char));
+  char *writePipe = malloc(11*sizeof(char));
   char id[3];
   for(i = 1; i <= numMonitors; i++){
     if((pid = fork()) < 0){
@@ -64,25 +82,75 @@ int main(int argc, char *argv[]){
     }
     if(pid == 0){
       sprintf(id, "%d", i);
-      if(execlp(path, path, id, NULL) < 0){
+      sprintf(readPipe, "pipes/%dr", i);
+      sprintf(writePipe, "pipes/%dw", i);
+      if(execlp(path, path, id, bufferSizeArg, readPipe, writePipe, NULL) < 0){
         perror("execlp");
         return 1;
       }
     }
   }
+  free(bufferSizeArg);
+  free(readPipe);
+  free(writePipe);
   int *write_file_descs = malloc(numMonitors*sizeof(int));
+  // Opening pipes created from monitors where parent will write
+  // the countries each monitor will process.
   for(i = 1; i <= numMonitors; i++){
+    sprintf(pipe_name, "pipes/%dw", i);
+    printf("About to open pipe %s for writing\n", pipe_name);
     write_file_descs[i-1] = open(pipe_name, O_WRONLY);
+    printf("Opened pipe %s for writing\n", pipe_name);
     if(write_file_descs[i-1] < 0){
-      perror("open");
+      perror("open write pipe");
     }
   }
+/*char *pipeReadBuffer = malloc(bufferSize*sizeof(char));
+  char *pipeWriteBuffer = malloc(bufferSize*sizeof(char));
+  DIR *inputDirectory = opendir(input_dir);
+  struct dirent *currentFolder = readdir(inputDirectory);
+  int charsCopied, countryLength = 0;
+  int roundRobin = 0;
+  while(currentFolder != NULL){
+    charsCopied = 0;
+    countryLength = strlen(currentFolder->d_name) + 1;
+    while(charsCopied < countryLength){
+      strncpy(pipeWriteBuffer, currentFolder->d_name + charsCopied, bufferSize);
+      if(write(write_file_descs[roundRobin], pipeWriteBuffer, bufferSize) < 0){
+        perror("write");
+        return 1;
+      }
+      charsCopied += bufferSize;
+    }
+    roundRobin++;
+    roundRobin = roundRobin % numMonitors;
+    currentFolder = readdir(inputDirectory);
+  }
+  closedir(inputDirectory);
+  fprintf(stderr, "Country names distributed");
+  char *endOfMessage = "END";
   for(i = 1; i <= numMonitors; i++){
+    charsCopied = 0;
+    countryLength = strlen(endOfMessage) + 1;
+    while(charsCopied < countryLength){
+      strncpy(pipeWriteBuffer, endOfMessage + charsCopied, bufferSize);
+      if(write(write_file_descs[roundRobin], pipeWriteBuffer, bufferSize) < 0){
+        perror("write");
+        return 1;
+      }
+      charsCopied += bufferSize;
+    }
+  } */
+  // waiting for children to exit...
+  printf("About to wait for my kids...\n");
+  for(i = 1; i <= numMonitors; i++){
+    printf("Waiting for kid no %d\n", i);
     if(wait(NULL) == -1){
       perror("wait");
       return 1;
     }
   }
+  // Closing and deleting all pipes
   for(i = 1; i <= numMonitors; i++){
     close(read_file_descs[i-1]);
     sprintf(pipe_name, "pipes/%dr", i);
@@ -95,6 +163,7 @@ int main(int argc, char *argv[]){
       perror("unlink");
     }
   }
+//  free(pipeWriteBuffer);
   free(read_file_descs);
   free(write_file_descs);
   free(pipe_name);
