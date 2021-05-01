e81b37431ea5b7e954f7818385072ac9416e558e
GKouv89 <gina101@windowslive.com>
2021-05-01 15:13:48 +0300

    A first draft of the date difference calculation in travelRequest

--- src/travelMonitor.c
+++ src/travelMonitor.c
@@ -13,6 +13,7 @@
 #include "../include/setofbfs.h"
 #include "../include/country.h"
 #include "../include/hashmap.h"
+#include "../include/dateOps.h"
 
 extern int errno;
 
@@ -313,6 +314,7 @@ int main(int argc, char *argv[]){
   char *command_name, *rest;
   char *answer = malloc(4*sizeof(char));
   char *date = malloc(12*sizeof(char));
+  char *dateOfTravel = malloc(12*sizeof(char));
   char *citizenID = malloc(5*sizeof(char));
   char *countryName = malloc(255*sizeof(char));
   char charsToWrite;
@@ -321,72 +323,85 @@ int main(int argc, char *argv[]){
     actual_length = getline(&command, &command_length, stdin);
     command_name = strtok_r(command, " ", &rest);
     if(strcmp(command_name, "/travelRequest") == 0){
-      if(sscanf(rest, "%s %s %s", citizenID, countryName, virusName) == 3){
-        curr_set = (setofbloomfilters *) find_node(setOfBFs_map, virusName);
-		    if(curr_set == NULL){
-          printf("No such virus: %s\n", virusName);
-          continue;
-        }else{
-          // Find which monitor handled the country's subdirectory
-          curr_country = (Country *) find_node(country_map, countryName);
-          if(curr_country == NULL){
-            printf("No such country: %s\n", countryName);
-            continue;
-          }
-          if(lookup_bf_vaccination(curr_set, curr_country->index, citizenID) == 1){
-            sprintf(request, "checkSkiplist %s %s", citizenID, virusName);
-            request_length = strlen(request);
-            if(write(write_file_descs[curr_country->index], &request_length, sizeof(char))  < 0){
-              perror("write checkSkiplist command length");
-            }
-            while(read(read_file_descs[curr_country->index], pipeReadBuffer, sizeof(char)) < 0);
-            charsCopied = 0;
-            while(charsCopied < request_length){
-              if(request_length - charsCopied < bufferSize){
-                charsToWrite = request_length - charsCopied;
-              }else{
-                charsToWrite = bufferSize;
-              }
-              strncpy(pipeWriteBuffer, request + charsCopied, charsToWrite*sizeof(char));
-              if(write(write_file_descs[curr_country->index], pipeWriteBuffer, charsToWrite) < 0){
-                perror("write checkSkiplist command chunk");
-              }else{
-                charsCopied += charsToWrite;
-              }
-            }
-            // Waiting for length of response.
-            while(read(read_file_descs[curr_country->index], &request_length, sizeof(char)) < 0);
-            if(write(write_file_descs[curr_country->index], "1", sizeof(char)) < 0){
-              perror("write answer length reception confirmation");
-            }
-            // Reading actual request, byte by byte.
-            charsCopied = 0;
-            memset(request, 0, 255*sizeof(char));
-            while(charsCopied < request_length){
-              if((charactersRead = read(read_file_descs[curr_country->index], pipeReadBuffer, bufferSize)) < 0){
-                continue;
-              }else{
-                strncat(request, pipeReadBuffer, charactersRead);
-                charsCopied += charactersRead;
-              }
-            }
-            // Send confirmation of answer reception.
-            if(write(write_file_descs[curr_country->index], "1", sizeof(char)) < 0){
-              perror("write answer reception confirmation");
-            }
-            if(request_length == 2){
-              printf("REQUEST REJECTED - YOU ARE NOT VACCINATED\n");
-            }else{
-              sscanf(request, "%s %s", answer, date);
-              printf("VACCINATED ON %s\n", date);
-            }
-          }else{
-            printf("REQUESTED REJECTED - YOU ARE NOT VACCINATED\n");
-          }
-        } 
-      }else{
-        printf("Bad arguments to /travelRequest. Try again.\n");
-      }
+      	if(sscanf(rest, "%s %s %s %s", citizenID, dateOfTravel, countryName, virusName) == 4){
+			if(dateFormatValidity(dateOfTravel) == -1){
+				printf("Invalid travel date format. Try again.\n");
+				continue;
+			}
+			curr_set = (setofbloomfilters *) find_node(setOfBFs_map, virusName);
+			if(curr_set == NULL){
+				printf("No such virus: %s\n", virusName);
+				continue;
+			}else{
+				// Find which monitor handled the country's subdirectory
+				curr_country = (Country *) find_node(country_map, countryName);
+				if(curr_country == NULL){
+					printf("No such country: %s\n", countryName);
+					continue;
+				}
+				if(lookup_bf_vaccination(curr_set, curr_country->index, citizenID) == 1){
+					sprintf(request, "checkSkiplist %s %s", citizenID, virusName);
+					request_length = strlen(request);
+					if(write(write_file_descs[curr_country->index], &request_length, sizeof(char))  < 0){
+					perror("write checkSkiplist command length");
+					}
+					while(read(read_file_descs[curr_country->index], pipeReadBuffer, sizeof(char)) < 0);
+					charsCopied = 0;
+					while(charsCopied < request_length){
+						if(request_length - charsCopied < bufferSize){
+							charsToWrite = request_length - charsCopied;
+						}else{
+							charsToWrite = bufferSize;
+						}
+						strncpy(pipeWriteBuffer, request + charsCopied, charsToWrite*sizeof(char));
+						if(write(write_file_descs[curr_country->index], pipeWriteBuffer, charsToWrite) < 0){
+							perror("write checkSkiplist command chunk");
+						}else{
+							charsCopied += charsToWrite;
+						}
+					}
+					// Waiting for length of response.
+					while(read(read_file_descs[curr_country->index], &request_length, sizeof(char)) < 0);
+					if(write(write_file_descs[curr_country->index], "1", sizeof(char)) < 0){
+						perror("write answer length reception confirmation");
+					}
+					// Reading actual request, byte by byte.
+					charsCopied = 0;
+					memset(request, 0, 255*sizeof(char));
+					while(charsCopied < request_length){
+						if((charactersRead = read(read_file_descs[curr_country->index], pipeReadBuffer, bufferSize)) < 0){
+							continue;
+						}else{
+							strncat(request, pipeReadBuffer, charactersRead);
+							charsCopied += charactersRead;
+						}
+					}
+					// Send confirmation of answer reception.
+					if(write(write_file_descs[curr_country->index], "1", sizeof(char)) < 0){
+						perror("write answer reception confirmation");
+					}
+					if(request_length == 2){
+						printf("REQUEST REJECTED - YOU ARE NOT VACCINATED\n");
+					}else{
+						sscanf(request, "%s %s", answer, date);
+						switch(dateDiff(dateOfTravel, date)){
+							case -1: printf("ERROR - TRAVEL DATE BEFORE VACCINATION DATE\n");
+									break;
+							case 0: printf("REQUEST ACCEPTED – HAPPY TRAVELS\n");
+									break;
+							case 1: printf("REQUEST REJECTED – YOU WILL NEED ANOTHER VACCINATION BEFORE TRAVEL DATE\n");
+									break;
+							default: printf("Invalid return value from date difference function.\n");
+									break; 
+						}
+					}
+				}else{
+					printf("REQUESTED REJECTED - YOU ARE NOT VACCINATED\n");
+				}
+			} 
+		}else{
+			printf("Bad arguments to /travelRequest. Try again.\n");
+		}
     }else if(strcmp(command_name, "/exit\n") == 0){
       for(i = 0; i < numMonitors; i++){
         kill(children_pids[i], 9);
@@ -406,15 +421,15 @@ int main(int argc, char *argv[]){
     }
   }
 	free(command);
-  free(answer);
-  free(date);
-  free(request);
+	free(answer);
+	free(date); free(dateOfTravel);
+	free(request);
 	free(citizenID);
 	free(countryName);
 	free(read_bloom_descs);
 	free(virusName);
-  free(children_pids);
-  // Closing and deleting all pipes
+  	free(children_pids);
+  	// Closing and deleting all pipes
 	for(i = 1; i <= numMonitors; i++){
 		close(read_file_descs[i-1]);
 		sprintf(pipe_name, "pipes/%dr", i);
